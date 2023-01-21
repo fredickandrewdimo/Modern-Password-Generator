fe1afa8882fd6566ccdcde7b542b9c626b2fb8ec
Andrew <fredickandrewdimo@gmail.com>
2023-01-21 17:41:06 +0800

    Add a simple function to generate

--- app.js
+++ app.js
@@ -1,7 +1,44 @@
+// Password Output Container
 const inputEl = document.getElementById("password-input");
-inputEl.value = "Hello World !";
 
+// Password Copy Element
 const copyBtn = document.getElementById("password-copy");
-copyBtn.addEventListener("click", function () {
-  console.log("Copied!");
+
+// Range Input
+const rangeInput = document.getElementById("character-length-range");
+
+let rangeValue = 0;
+
+rangeInput.addEventListener("input", function () {
+  rangeValue = this.value;
+  console.log(rangeValue);
+
+  // Display the range
+  const lengthValue = document.getElementById("character-length-value");
+  lengthValue.innerText = rangeValue;
+
+  // generate a password with the selected length
+  let password = generatePassword(rangeValue);
+  console.log(password);
+
+  inputEl.value = password;
 });
+
+// generate a simple password
+function generatePassword(length) {
+  const upperCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+  let password = "";
+
+  for (let i = 0; i < length; i++) {
+    password += upperCaseLetters.charAt(
+      Math.floor(Math.random() * upperCaseLetters.length)
+    );
+  }
+
+  return password;
+}
+
+// const lowerCaseLetters = "abcdefghijklmnopqrstuvwxyz";
+// const numbers = "0123456789";
+// const sysmbols = "!@#$%^&*()_+-=[]{}|;':,.<>/?`~";
