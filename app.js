60e1f21008041bf8f88f0ced24d7ef1c44daafda
Andrew <fredickandrewdimo@gmail.com>
2023-01-21 21:00:14 +0800

    finished the include and length of the passwor

--- app.js
+++ app.js
@@ -1,44 +1,122 @@
 // Password Output Container
 const inputEl = document.getElementById("password-input");
 
-// Password Copy Element
-const copyBtn = document.getElementById("password-copy");
-
 // Range Input
 const rangeInput = document.getElementById("character-length-range");
 
+// Length of the Password
 let rangeValue = 0;
 
 rangeInput.addEventListener("input", function () {
   rangeValue = this.value;
-  console.log(rangeValue);
 
   // Display the range
   const lengthValue = document.getElementById("character-length-value");
   lengthValue.innerText = rangeValue;
+});
 
-  // generate a password with the selected length
+// Generates the Password when the button is clicked
+
+let chars = [];
+
+const generateBtn = document.getElementById("generate-btn");
+
+generateBtn.addEventListener("click", function () {
+  const lowerCaseLetters = "abcdefghijklmnopqrstuvwxyz";
+  const lowerCaseSplit = lowerCaseLetters.split("");
+
+  const upperCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const upperCaseSplit = upperCaseLetters.split("");
+
+  const numbers = "0123456789";
+  const numbersSplit = numbers.split("");
+
+  const symbols = "!@#$%^&*()_+-=[]{}|;':,.<>/?`~";
+  const symbolsSplit = symbols.split("");
+
+  // Include LowerCase Letter
+  const includeLowerCase = document.getElementById(
+    "include-lowercase-checkbox"
+  );
+
+  // Include UpperCase Letters
+  const includeUpperCase = document.getElementById(
+    "include-uppercase-checkbox"
+  );
+
+  // Include Numbers
+  const includeNumbers = document.getElementById("include-numbers-checkbox");
+
+  // Include Symbols
+  const includeSymbols = document.getElementById("include-symbols-checkbox");
+
+  if (includeLowerCase.checked) {
+    chars = chars.concat(lowerCaseSplit);
+  } else {
+    for (let i = 0; i < lowerCaseSplit.length; i++) {
+      let index = chars.indexOf(lowerCaseSplit[i]);
+      if (index !== -1) {
+        chars.splice(index, 1);
+      }
+    }
+  }
+
+  if (includeUpperCase.checked) {
+    chars = chars.concat(upperCaseSplit);
+  } else {
+    for (let i = 0; i < upperCaseSplit.length; i++) {
+      let index = chars.indexOf(upperCaseSplit[i]);
+      if (index !== -1) {
+        chars.splice(index, 1);
+      }
+    }
+  }
+
+  if (includeNumbers.checked) {
+    chars = chars.concat(numbersSplit);
+  } else {
+    for (let i = 0; i < numbersSplit.length; i++) {
+      let index = chars.indexOf(numbersSplit[i]);
+      if (index !== -1) {
+        chars.splice(index, 1);
+      }
+    }
+  }
+
+  if (includeSymbols.checked) {
+    chars = chars.concat(symbolsSplit);
+  } else {
+    for (let i = 0; i < symbolsSplit.length; i++) {
+      let index = chars.indexOf(symbolsSplit[i]);
+      if (index !== -1) {
+        chars.splice(index, 1);
+      }
+    }
+  }
+
   let password = generatePassword(rangeValue);
-  console.log(password);
-
   inputEl.value = password;
 });
 
-// generate a simple password
-function generatePassword(length) {
-  const upperCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+// Random Characters
 
+function generatePassword(length) {
   let password = "";
 
   for (let i = 0; i < length; i++) {
-    password += upperCaseLetters.charAt(
-      Math.floor(Math.random() * upperCaseLetters.length)
-    );
+    password += chars[Math.floor(Math.random() * chars.length)];
   }
 
   return password;
 }
 
-// const lowerCaseLetters = "abcdefghijklmnopqrstuvwxyz";
-// const numbers = "0123456789";
-// const sysmbols = "!@#$%^&*()_+-=[]{}|;':,.<>/?`~";
+// Copy the password to the clipboard
+const copyBtn = document.getElementById("password-copy");
+
+copyBtn.addEventListener("click", function () {
+  let generatedPassword = inputEl.value;
+  navigator.clipboard.writeText(generatedPassword);
+});
+
+/* Set the length of the password you want to generate, then i want that function will just execute at the last value that the range gave, before it generates a password they need to here a click event first.
+ */
