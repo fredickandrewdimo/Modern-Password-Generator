7d71aafbc4802155f592c7fa601061aa522a0802
Andrew <fredickandrewdimo@gmail.com>
2023-01-23 21:40:34 +0800

    Structured the code and added comments

--- app.js
+++ app.js
@@ -1,58 +1,76 @@
-// Password Output Container
+// Get reference to input password element
 const inputEl = document.getElementById("password-input");
 
-// Range Input
+// Get the length through range input element //
+
+// Get reference to range input element
 const rangeInput = document.getElementById("character-length-range");
 
-// Length of the Password
+// rangeValue variable
 let rangeValue = 0;
 
+// Listen for input event on range input
 rangeInput.addEventListener("input", function () {
+  // Update rangeValue variable
   rangeValue = this.value;
-  // Display the range
+
+  // Update inner text of another element with the current rangeValue
   const lengthValue = document.getElementById("character-length-value");
   lengthValue.innerText = rangeValue;
-  //   console.log(rangeValue);
 });
 
-// Generates the Password when the button is clicked
+// Generates the Password when the button is clicked //
+
 let chars = [];
 let password;
 
+// Get reference to generate button element
 const generateBtn = document.getElementById("generate-btn");
 
+// Define a string of lowercase letters
+const lowerCaseLetters = "abcdefghijklmnopqrstuvwxyz";
+// Split the string of lowercase letters
+const lowerCaseSplit = lowerCaseLetters.split("");
+
+// Define a string of uppercase letters
+const upperCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+// Split the string of uppercase letters
+const upperCaseSplit = upperCaseLetters.split("");
+
+// Define a string of numbers
+const numbers = "0123456789";
+// Split the string of numbers
+const numbersSplit = numbers.split("");
+
+// Define a string of special characters
+const symbols = "!@#$%^&*()_+-=[]{}|;':,.<>/?`~";
+// Split the string of special characters
+const symbolsSplit = symbols.split("");
+
+// Button listen to a click event
 generateBtn.addEventListener("click", function () {
-  const lowerCaseLetters = "abcdefghijklmnopqrstuvwxyz";
-  const lowerCaseSplit = lowerCaseLetters.split("");
-
-  const upperCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  const upperCaseSplit = upperCaseLetters.split("");
-
-  const numbers = "0123456789";
-  const numbersSplit = numbers.split("");
-
-  const symbols = "!@#$%^&*()_+-=[]{}|;':,.<>/?`~";
-  const symbolsSplit = symbols.split("");
-
-  // Include LowerCase Letter
+  // Get a reference to the "include-lowercase-checkbox" element
   const includeLowerCase = document.getElementById(
     "include-lowercase-checkbox"
   );
 
-  // Include UpperCase Letters
+  // Get a reference to the "include-uppercase-checkbox" element
   const includeUpperCase = document.getElementById(
     "include-uppercase-checkbox"
   );
 
-  // Include Numbers
+  // Get a reference to the "include-numbers-checkbox" element
   const includeNumbers = document.getElementById("include-numbers-checkbox");
 
-  // Include Symbols
+  // Get a reference to the "include-symbols-checkbox" element
   const includeSymbols = document.getElementById("include-symbols-checkbox");
 
+  // check if includeLowerCase checkbox is checked
   if (includeLowerCase.checked) {
+    // If checked, add the lowerCaseSplit characters to the chars array
     chars = chars.concat(lowerCaseSplit);
   } else {
+    // If not checked, remove the lowerCaseSplit characters from the chars array
     for (let i = 0; i < lowerCaseSplit.length; i++) {
       let index = chars.indexOf(lowerCaseSplit[i]);
       if (index !== -1) {
@@ -61,9 +79,12 @@ generateBtn.addEventListener("click", function () {
     }
   }
 
+  // check if includeUpperCase checkbox is checked
   if (includeUpperCase.checked) {
+    // If checked, add the upperCaseSplit characters to the chars array
     chars = chars.concat(upperCaseSplit);
   } else {
+    // If not checked, remove the upperCaseSplit characters from the chars array
     for (let i = 0; i < upperCaseSplit.length; i++) {
       let index = chars.indexOf(upperCaseSplit[i]);
       if (index !== -1) {
@@ -72,9 +93,12 @@ generateBtn.addEventListener("click", function () {
     }
   }
 
+  // check if includeNumbers checkbox is checked
   if (includeNumbers.checked) {
+    // If checked, add the numbersSplit characters to the chars array
     chars = chars.concat(numbersSplit);
   } else {
+    // If not checked, remove the numbersSplit characters from the chars array
     for (let i = 0; i < numbersSplit.length; i++) {
       let index = chars.indexOf(numbersSplit[i]);
       if (index !== -1) {
@@ -83,9 +107,12 @@ generateBtn.addEventListener("click", function () {
     }
   }
 
+  // check if includeSymbols checkbox is checked
   if (includeSymbols.checked) {
+    // If checked, add the symbolsSplit characters to the chars array
     chars = chars.concat(symbolsSplit);
   } else {
+    // If not checked, remove the symbolsSplit characters from the chars array
     for (let i = 0; i < symbolsSplit.length; i++) {
       let index = chars.indexOf(symbolsSplit[i]);
       if (index !== -1) {
@@ -94,10 +121,13 @@ generateBtn.addEventListener("click", function () {
     }
   }
 
+  // Call a generatePassword function and send a rangeValue
   password = generatePassword(rangeValue);
-  inputEl.value = password;
-  //   console.log(password);
 
+  // Declares the inputEl value is the password
+  inputEl.value = password;
+
+  // Array of container elements
   const containers = [
     verticalBarContainer1,
     verticalBarContainer2,
@@ -105,16 +135,17 @@ generateBtn.addEventListener("click", function () {
     verticalBarContainer4,
   ];
 
+  // loop to iterate through each element in the containers array
   for (let i = 0; i < containers.length; i++) {
+    // Remove the class "vertical-bar-yellow" from the current element
     containers[i].classList.remove("vertical-bar-yellow");
   }
 
+  // Test the strength of the password a zxcvbn library
   let strength = zxcvbn(password);
-  console.log(strength.score);
 
   if (strength.score === 0) {
     strengthName.innerText = "Very Weak";
-
     console.log("Very Weak");
   } else if (strength.score === 1) {
     strengthName.innerText = "Weak";
@@ -159,15 +190,18 @@ const verticalBarContainer4 = document.getElementById(
   "vertical-bar-container-4"
 );
 
-// Random Characters
-
+// Generates a Random Password with specified length
 function generatePassword(length) {
+  // Initialize an empty string to store the password
   let password = "";
 
+  //   Loop for the specified length
   for (let i = 0; i < length; i++) {
+    // Add a randomly selected character from the chars array to the password string
     password += chars[Math.floor(Math.random() * chars.length)];
   }
 
+  // Return the generated password
   return password;
 }
 
@@ -175,30 +209,5 @@ function generatePassword(length) {
 const copyBtn = document.getElementById("password-copy");
 
 copyBtn.addEventListener("click", function () {
-  let generatedPassword = inputEl.value;
-  navigator.clipboard.writeText(generatedPassword);
+  navigator.clipboard.writeText(inputEl.value);
 });
-
-// strength-value-name
-
-// console.log(password);
-
-// const verticalBar1 = document.getElementById("vertical-bar-container-1");
-// verticalBar1.classList.add("vertical-bar-yellow");
-
-/* Set the length of the password you want to generate, then i want that function will just execute at the last value that the range gave, before it generates a password they need to here a click event first.
- */
-
-/* 
-
-
-    if the password length is greater than 10 and includes uppercase and     lowercase the result is medium
-        Two bars of containers turned into yellow
-        innerText MEDIUM
-
-    else if password length is greater than 12 and includes uppercase and lowercase, numbers and symbols the result is strong
-        Three bars of containers turned into yellow
-        innerText STRONG
-
-    else the result is weak
-*/
